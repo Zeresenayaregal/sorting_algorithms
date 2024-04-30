2405f7eb58b468d1a29ba457bcd2b631d44be175
Zeresenay <zeresenayyaregal4@gmail.com>
2024-04-30 18:45:20 +0300

    Local files pushed try 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,3 +25,19 @@ void bubble_sort(int *array, size_t size)
 		}
 	}
 }
+
+/**
+ * swp - Function that swaps two values
+ *
+ * @num1: Fisrt value
+ * @num2: Second value
+ * Return: 0
+ */
+void swp(int *num1, int *num2)
+{
+	int temp;
+
+	temp = *num2;
+	*num2 = *num1;
+	*num1 = temp;
+}
