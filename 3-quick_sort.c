363a4ac209d941bc4f69dc3fe66ccd5a6609a7d9
Zeresenay <zeresenayyaregal4@gmail.com>
2024-04-30 18:52:05 +0300

    Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -82,3 +82,19 @@ void ob_sort(int array[], size_t size, int strt)
 	if (!init)
 		print_array(a, b);
 }
+
+/**
+ * swp - Function that swaps two values
+ *
+ * @num1: Fisrt value
+ * @num2: Second value
+ * Return: 0
+ */
+void swp(int *num1, int *num2)
+{
+	int temp;
+
+	temp = *num2;
+	*num2 = *num1;
+	*num1 = temp;
+}
